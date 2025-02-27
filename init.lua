f7b2f681da2e19e0d7e0f9a89941d390d0844eba
dmt-machine-elf <199597028+dmt-machine-elf@users.noreply.github.com>
2025-02-27 19:41:24 +0700

    8 space indent

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.cmd([[highlight TabLineFill guibg=#282828 guifg=#928374]])
+vim.cmd([[highlight TabLine guibg=#3c3836 guifg=#ebdbb2]])
+vim.cmd([[highlight TabLineSel guibg=#3c3836 guifg=#ebdbb2]])
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,38 +1,38 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-        local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-        local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-        if vim.v.shell_error ~= 0 then
-                vim.api.nvim_echo({
-                        { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-                        { out, "WarningMsg" },
-                        { "\nPress any key to exit..." },
-                }, true, {})
-                vim.fn.getchar()
-                os.exit(1)
-        end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-        spec = {
-                { "LazyVim/LazyVim", import = "lazyvim.plugins" }, -- standard LazyVim setup
-                { import = "plugins" }, -- custom plugin overrides
+    spec = {
+        { "LazyVim/LazyVim", import = "lazyvim.plugins" }, -- standard LazyVim setup
+        { import = "plugins" }, -- custom plugin overrides
+    },
+    defaults = {
+        lazy = false, -- load custom plugins on startup
+        version = false, -- use latest commit of plugins
+    },
+    install = { colorscheme = { "githum-nvim-theme" } }, -- colorschemes
+    checker = { enabled = true, notify = false }, -- auto plugin updates
+    performance = {
+        rtp = {
+            disabled_plugins = {
+                "gzip",
+                "tarPlugin",
+                "tohtml",
+                "zipPlugin", -- remove unnecessary plugins
+            },
         },
-        defaults = {
-                lazy = false, -- load custom plugins on startup
-                version = false, -- use latest commit of plugins
-        },
-        install = { colorscheme = { "githum-nvim-theme" } }, -- colorschemes
-        checker = { enabled = true, notify = false }, -- auto plugin updates
-        performance = {
-                rtp = {
-                        disabled_plugins = {
-                                "gzip",
-                                "tarPlugin",
-                                "tohtml",
-                                "zipPlugin", -- remove unnecessary plugins
-                        },
-                },
-        },
+    },
 })
